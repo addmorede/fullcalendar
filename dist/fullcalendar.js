e030aedb3bdd8c0b648d8914b6c7b92228c5db14
Jan Zaeske <mail@jzaeske.de>
2019-01-04 10:30:31 +0100

    Update fullcalendar.js

--- dist/fullcalendar.js
+++ dist/fullcalendar.js
@@ -1,4 +1,4 @@
-c/*!
+/*!
  * FullCalendar v3.9.0
  * Docs & License: https://fullcalendar.io/
  * (c) 2018 Adam Shaw
@@ -13838,11 +13838,4 @@ var TimeGridEventRenderer = /** @class */ (function (_super) {
                 '') +
             '</div>' +
             '<div class="fc-bg"/>' +
-            /* TODO: write CSS for this
-            (isResizableFromStart ?
-              '<div class="fc-resizer fc-start-resizer" />' :
-              ''
-              ) +
-            */
-            (isResizableFromEnd ?
-                '<div class="fc
+            /* TODO: write CSS for thi
